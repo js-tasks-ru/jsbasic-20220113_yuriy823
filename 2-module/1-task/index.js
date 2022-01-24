9d5108d185beb476ffea99d3aa6c473536281e7e
Yury <borisov@airlan.ru>
2022-01-25 00:20:39 +0300

    Занятие 2: Объекты. Решения задач.

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
-}
+  let sum = 0;
+  for (let key in salaries) {
+    let sal = salaries[key];
+    if (typeof(sal) == 'number' && isFinite(sal) && !isNaN(sal)) {
+      sum += sal;
+    }
+  };
+  return sum;
+};
