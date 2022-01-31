69630ba83affb860dca17e0d781d5de8ace92216
Yury <borisov@airlan.ru>
2022-01-31 17:29:34 +0300

    Занятие 3: Функции и массивы. Решение задач. Устранение замечаний.

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,5 +1,5 @@
 function showSalary(users, age) {
-  let str = '';
-  for (let item of users) str += item.age<=age ? (str=='' ? '': '\n') + item.name +', '+ item.balance : '';
-  return str;
+  let str;
+  users.filter(item => item.age <= age).forEach(item => str = (str==undefined ? '' : str + '\n') + item.name +', '+ item.balance);  
+  return str; // возвращает undefined, а не пустую строку, если подходящих объектов нет 
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,12 +1,12 @@
 function getMinMax(str) {
+  function selectMinMax( itm ) {
+    (itm < arr_min || typeof(arr_min)=='undefined') ? arr_min = +itm : null;
+    (itm > arr_max || typeof(arr_max)=='undefined') ? arr_max = +itm : null;  
+  };
   let arr = str.split(' '), arr_min, arr_max;
   arr.forEach(itm => isFinite( itm ) ? selectMinMax( itm ) : null);
   return {
   min: arr_min,
   max: arr_max
-  };
-  function selectMinMax( itm ) {
-    (itm < arr_min || typeof(arr_min)=='undefined') ? arr_min = +itm : null;
-    (itm > arr_max || typeof(arr_max)=='undefined') ? arr_max = +itm : null;  
   };
 }
