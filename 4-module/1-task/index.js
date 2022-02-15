5f24cf7e39ea4f61e23d5de8e1b83096b35e0899
Yury <borisov@airlan.ru>
2022-02-15 15:47:27 +0300

    Доработка задач 1, 3 модуля 4 по замечаниям преподавателя.

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,18 +1,24 @@
 function makeFriendsList(friends) {
-  let ul = document.createElement('ul'), li;
+  let ul = document.createElement('ul');
+
   for (let friend of friends) {
-    let names = '';
-    for (let name in friend) {
-      names += (names=='' ? '' : ' ') + friend[name]
-    }
-    if (names !== '') {
+    let names = friend.firstName +' '+ friend.lastName;
+    if (names !== ' ') {
       ul.append('\n   ');
-      li = document.createElement('li');
+      let li = document.createElement('li');
       li.append(names);
       ul.append(li);
     }
   }
+
   ul.append('\n');
   // console.log(ul.outerHTML);
   return ul;
 }
+/* Перенос строки добавляется, чтобы результирующий консольный HTML имел вид, как в задачнике:
+<ul>
+   <li>Artsiom Mezin</li>
+   <li>Ilia Kantor</li>
+   <li>Christopher Michael</li>
+</ul>
+*/
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,17 +1,40 @@
 function highlight(table) {
-  let cellStrUp = (itm) => itm.firstChild.data.trim().toUpperCase(); // cell String Upper case
+  // Значение ячейки таблицы в верхнем регистре / cell String Upper case
+  let cellStrUp = (itm) => itm.firstChild.data.trim().toUpperCase(); 
+  // Массив заголовка таблицы thead
   let thead_td = Array.from(table.querySelectorAll('thead td'));
-  let colNmb = (hName) => thead_td.findIndex(item => cellStrUp(item) === hName.toUpperCase()); // номер столбца с заголовком hName
+  // Определение номера столбца по заголовку hName
+  let colNmb = (hName) => thead_td.findIndex(item => cellStrUp(item) === hName.toUpperCase()); 
+  
   for (tRow of table.rows) 
     if (!tRow.closest('thead')) { // пропустить заголовок
-      tRow.hidden = tRow.cells[colNmb('Status')].dataset.available == undefined;
-      if (!tRow.hidden)      
-        tRow.classList.add(tRow.cells[colNmb('Status')].dataset.available.toLowerCase()==='true' ? 'available' : 'unavailable'); // getAttribute('data-available')
+      // hidden
+      tRow.hidden = (tRow.cells[colNmb('Status')].dataset.available == undefined);
+      // available / unavailable
+      if (!tRow.hidden) {
+        if (tRow.cells[colNmb('Status')].dataset.available.toLowerCase()==='true') {
+          tRow.classList.add('available');
+        } else {
+          tRow.classList.add('unavailable');
+        }
+      }      
+      // Gender 
       let m_or_f = cellStrUp(tRow.cells[colNmb('Gender')]);
-      m_or_f = m_or_f==='M' ? 'male' : m_or_f==='F' ? 'female' : null; // в общем случае m_or_f может иметь другие значения кроме m,f
+      switch(m_or_f) {
+        case 'M':  
+          m_or_f = 'male';
+          break;
+        case 'F':  
+          m_or_f = 'female';
+          break;
+        default:
+          m_or_f = null; // если в столбце Gender будет значение отличное от m,f
+      }
       tRow.classList.add(m_or_f);
+      // Age
       let age = cellStrUp(tRow.cells[colNmb('Age')]);
-      if (isFinite(age) && age < 18 )
-        tRow.style = "text-decoration: line-through"; 
+      if (isFinite(age) && age < 18 ) {
+        tRow.style = "text-decoration: line-through";
+      }   
     }
 }
