51fcc057fde235cc3bb217fdb1d77a37251433d9
Yury <borisov@airlan.ru>
2022-03-07 17:34:12 +0300

    Занятие 5: Обработчики событий. Решения задач.

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,10 @@
 function hideSelf() {
-  // ваш код...
+
+  document.onclick = hideButton; 
+
+  function hideButton(e) {
+    if (document.querySelector('.hide-self-button') == e.target) { 
+      e.target.hidden = true;
+    }  
+  }
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,36 @@
 function initCarousel() {
-  // ваш код...
+  let slider =  document.querySelector('.carousel__inner'); // слайдер
+  let slidesNumb = document.querySelectorAll('.carousel__inner .carousel__slide').length; // кол-во слайдов в ленте
+  let arrrowLeft = document.querySelector('.carousel__arrow_left');
+  let arrrowRight = document.querySelector('.carousel__arrow_right');
+  let pos = 0; // текущая позиция == номер слайда - 1
+
+  arrowsShowHide();   
+
+  document.onclick = shiftPicture;
+  
+  function shiftPicture(event) {
+    let shiftVal = 0;
+    let slideWidth = slider.offsetWidth; // ширина одного слайда
+    
+    let course = ''; // направление движения: L / R
+    if (event.target.closest('.carousel__arrow_right')) course = 'L';
+    else if (event.target.closest('.carousel__arrow_left')) course = 'R';
+    
+    if (course) {
+      shiftVal = slideWidth * ((course === 'L') ? ++pos : --pos);
+      let trans = 'translateX(-' + shiftVal + 'px)';
+      slider.style.transform = trans;
+
+      arrowsShowHide();
+    };
+  };
+
+  function arrowsShowHide() { // отображение кнопок
+    if (pos === 0) arrrowLeft.style.display = 'none';
+    else arrrowLeft.style.display = '';
+    if (pos === slidesNumb-1) arrrowRight.style.display = 'none';
+    else arrrowRight.style.display = '';
+  };
+
 }
