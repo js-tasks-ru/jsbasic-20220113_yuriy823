299e6224c7e40b3133412f121064871ed1ca2100
Yury <borisov@airlan.ru>
2022-03-25 22:08:02 +0300

    Занятие 6: JS классы и компоненты. Решения задач.

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,51 @@
  *
  */
 export default class UserTable {
-  constructor(rows) {
+
+  #myTable = null; // приватное св-во 
+  
+  constructor( rows ) {
+    const headers = ['Имя', 'Возраст', 'Зарплата', 'Город']; // заголовки столбцов
+
+    this.#myTable = document.createElement('table');
+    
+    // Заголовок таблицы
+    let myTHead = document.createElement('thead');
+    let myTR = document.createElement('tr');
+    for (let i=0; i <= headers.length; i++) {
+      let myTH = document.createElement('th');
+      myTH.append(i < headers.length ? headers[i] : '');
+      myTR.append(myTH);
+    };
+    myTHead.append(myTR);
+    this.#myTable.append(myTHead);
+    
+    // Содержание таблицы
+    let myTBody = document.createElement('tbody');
+    for (let row of rows) {
+      myTR = document.createElement('tr');    
+      for (let k in row) {
+        let myTD = document.createElement('td');
+        myTD.append(row[k]);
+        myTR.append(myTD);
+      };
+      // Кнопка "X" в конце строки
+      let myBotton = document.createElement('button');
+      myBotton.append('X');
+      myBotton.onclick = this.lineDel;
+      let myTD = document.createElement('td');
+      myTD.append(myBotton);
+      myTR.append(myTD);
+      myTBody.append(myTR);
+    }; 
+    this.#myTable.append(myTBody);   
   }
-}
+  get elem() { // ReadOnly поле, т.к. нет метода set
+    return this.#myTable;
+  } 
+  lineDel(event) { // удаление строки таблицы
+    let elem = event.target.closest('tr');
+    if (elem) elem.remove(); // если не пусто и Ok
+  }
+
+}  
--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,53 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class ProductCard {
+
+  #card = null;
+  #prodId = null;
+
   constructor(product) {
+    this.#card = createElement(`
+    <div class="card">
+      <div class="card__top">
+        <img src="/assets/images/products/picture.png" class="card__image" alt="product">
+        <span class="card__price">price</span>
+      </div>
+      <div class="card__body">
+        <div class="card__title">name</div>
+        <button type="button" class="card__button">
+          <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+        </button>
+      </div>
+    </div>`);
+    // name
+    let elem = this.#card.querySelector('.card__title');
+    elem.innerHTML = product.name;
+    // price
+    elem = this.#card.querySelector('.card__price');
+    elem.innerHTML = '€' + product.price.toFixed(2);
+    // image
+    elem = this.#card.querySelector('.card__image');
+    let src = elem.getAttribute('src');
+    src = src.replace('picture.png', product.image);
+    elem.setAttribute('src', src);
+    // button click
+    elem = this.#card.querySelector('.card__button');
+    elem.onclick = this.productAdd;
+    // id
+    elem.id = product.id;
+  }
+
+  get elem() {
+    return this.#card;
+  }
+
+  productAdd(event) {
+    let newEvent = new CustomEvent("product-add", 
+    { // имя события должно быть именно "product-add"
+      detail: event.target.closest('.card__button').id, // Уникальный идентификатора товара из объекта товара
+      bubbles: true // это событие всплывает - это понадобится в дальнейшем
+    });
+    let elem = event.target.closest('.card'); // корневой элемент
+    elem.dispatchEvent(newEvent);
   }
 }
